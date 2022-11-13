dbbe6e751804fc78983b90157c68546abf097fb3
Henry Lin <henry.ys.lin@gmail.com>
2022-11-13 12:48:49 +0800

    Add regex filter for course code

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import ical from 'node-ical';
-import icalGenerator, { ICalCalendarMethod, ICalEventClass } from 'ical-generator';
+import icalGenerator, { ICalCalendarMethod, ICalEventClass, ICalEventData } from 'ical-generator';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const COURSE_CODE_MAP: { [k: string]: string | undefined } = {
@@ -24,10 +24,16 @@ function isCalendar(obj: ical.CalendarComponent): obj is ical.VCalendar {
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const link = req.query.link;
+  const filter = req.query.filter;
 
   if (typeof link !== 'string') {
     res.status(400).send('Bad request: link is not a string');
   }
+  if (typeof filter !== 'string' && filter !== undefined) {
+    res.status(400).send('Bad request: filter is not a string and is not empty');
+  }
+
+  const filterRegex = filter === undefined ? null : new RegExp(filter as string, 'i');
 
   // download file content
 
@@ -79,62 +85,69 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   calendar.description('Class and exam schedule from MFPO, processed to improve formatting.');
 
   calendar.events(
-    vEvents.map(event => {
-      let courseCode: string | undefined;
-      let sessionType: string | undefined;
-      const match = /^(\w{4}\d{4}|MED3-EVT) \(([\w- ()]+?)\)/.exec(event.summary);
-      if (match) {
-        courseCode = match[1];
-        sessionType = match[2];
-      } else if (event.summary) {
-        const courseCodeMatch = /^(\w{4}\d{4}|MED3-EVT)/.exec(event.summary);
-        if (courseCodeMatch) {
-          courseCode = courseCodeMatch[1];
+    vEvents
+      .map(event => {
+        let courseCode: string | undefined;
+        let sessionType: string | undefined;
+        const match = /^(\w{4}\d{4}|MED3-EVT) \(([\w- ()]+?)\)/.exec(event.summary);
+        if (match) {
+          courseCode = match[1];
+          sessionType = match[2];
+        } else if (event.summary) {
+          const courseCodeMatch = /^(\w{4}\d{4}|MED3-EVT)/.exec(event.summary);
+          if (courseCodeMatch) {
+            courseCode = courseCodeMatch[1];
+          }
+          if (event.summary.toLowerCase().includes('(assessment)')) {
+            sessionType = 'Assessment';
+          } else if (event.summary.toLowerCase().includes('(e-lecture')) {
+            sessionType = 'E-lecture';
+          } else if (event.summary.toLowerCase().includes('(self study)')) {
+            sessionType = 'Self study';
+          } else if (event.summary.toLowerCase().includes('(self-learning')) {
+            sessionType = 'Self learning';
+          } else if (event.summary.toLowerCase().includes('(visit)')) {
+            sessionType = 'Visit';
+          }
         }
-        if (event.summary.toLowerCase().includes('(assessment)')) {
-          sessionType = 'Assessment';
-        } else if (event.summary.toLowerCase().includes('(e-lecture')) {
-          sessionType = 'E-lecture';
-        } else if (event.summary.toLowerCase().includes('(self study)')) {
-          sessionType = 'Self study';
-        } else if (event.summary.toLowerCase().includes('(self-learning')) {
-          sessionType = 'Self learning';
-        } else if (event.summary.toLowerCase().includes('(visit)')) {
-          sessionType = 'Visit';
-        }
-      }
 
-      if (sessionType) {
-        if (sessionType.toLowerCase().startsWith('dissection')) {
-          sessionType = 'Dissection';
-        } else if (sessionType.toLowerCase().startsWith('flipped classroom')) {
-          sessionType = 'Flipped classroom';
-        }
-      }
-
-      let summary;
-      if (courseCode) {
         if (sessionType) {
-          summary = `${COURSE_CODE_MAP[courseCode] ?? courseCode} - ${sessionType}`;
-        } else {
-          summary = `${COURSE_CODE_MAP[courseCode] ?? courseCode} - UNKNOWN`;
+          if (sessionType.toLowerCase().startsWith('dissection')) {
+            sessionType = 'Dissection';
+          } else if (sessionType.toLowerCase().startsWith('flipped classroom')) {
+            sessionType = 'Flipped classroom';
+          }
         }
-      } else {
-        summary = 'UNKNOWN - ' + event.summary;
-      }
 
-      return {
-        id: event.uid,
-        class: event.class.trim() as ICalEventClass,
-        stamp: event.dtstamp,
-        start: event.start,
-        end: event.end,
-        location: event.location,
-        sequence: Number(event.sequence),
-        summary,
-        description: event.summary,
-      };
-    })
+        let summary;
+        if (courseCode) {
+          if (sessionType) {
+            summary = `${COURSE_CODE_MAP[courseCode] ?? courseCode} - ${sessionType}`;
+          } else {
+            summary = `${COURSE_CODE_MAP[courseCode] ?? courseCode} - UNKNOWN`;
+          }
+        } else {
+          courseCode = 'UNKNOWN';
+          summary = `${courseCode} - ${event.summary}`;
+        }
+
+        if (filterRegex && !filterRegex.test(summary)) {
+          return null;
+        }
+
+        return {
+          id: event.uid,
+          class: event.class.trim() as ICalEventClass,
+          stamp: event.dtstamp,
+          start: event.start,
+          end: event.end,
+          location: event.location,
+          sequence: Number(event.sequence),
+          summary,
+          description: event.summary,
+        };
+      })
+      .filter(Boolean) as ICalEventData[]
   );
 
   res.setHeader('Content-Type', 'text/calendar').send(calendar.toString());
